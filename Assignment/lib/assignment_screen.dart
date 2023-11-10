e269f219e629a838b774652acb2b08df199e00f5
Priyanshu Mallick <priyanshumallick123456@gmail.com>
2023-11-10 17:27:50 +0530

    2nd Submission

--- Assignment/lib/assignment_screen.dart
+++ Assignment/lib/assignment_screen.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
 
-class AssignmentScrren extends StatefulWidget {
-  const AssignmentScrren({super.key});
+class AssignmentScreen extends StatefulWidget {
+  const AssignmentScreen({Key? key}) : super(key: key);
 
   @override
-  State<AssignmentScrren> createState() => _AssignmentScrrenState();
+  State<AssignmentScreen> createState() => _AssignmentScreenState();
 }
 
-class _AssignmentScrrenState extends State<AssignmentScrren> with SingleTickerProviderStateMixin{
-
+class _AssignmentScreenState extends State<AssignmentScreen>
+    with SingleTickerProviderStateMixin {
   late TabController _tabController;
 
+  @override
   void initState() {
     super.initState();
     _tabController = TabController(length: 3, vsync: this, initialIndex: 1);
@@ -24,8 +25,6 @@ class _AssignmentScrrenState extends State<AssignmentScrren> with SingleTickerPr
 
   @override
   Widget build(BuildContext context) {
-
-    final screenWidth = MediaQuery.of(context).size.width;
     final screenHeight = MediaQuery.of(context).size.height;
 
     return Scaffold(
@@ -43,54 +42,45 @@ class _AssignmentScrrenState extends State<AssignmentScrren> with SingleTickerPr
             color: Colors.black,
           ),
           onPressed: () {
-            // Add navigation logic here
+            Navigator.of(context).pop();
           },
         ),
       ),
-      body: SingleChildScrollView(
-        physics: BouncingScrollPhysics(),
-        child: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.all(16.0),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(30.0),
-                child: Image.asset(
-                  'assets/ex.png', // Replace with the correct path to your image
-                  width: double.infinity,
-                  height: 200, // Adjust the height as needed
-                  fit: BoxFit.cover,
+      body: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(30.0),
+                  child: Image.asset(
+                    'assets/ex.png',
+                    width: double.infinity,
+                    height: 200,
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
             ),
-            Padding(
-              padding:EdgeInsets.symmetric(
-                  horizontal: screenWidth * 0.04,
-                  vertical: screenHeight * 0.001),
-              child: Container(
-                height: screenHeight * 0.06,
-                // Set the desired height for the TabBar
-                padding: EdgeInsets.all(screenWidth * 0.008),
-                decoration: BoxDecoration(
-                  color: Colors.black12,
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                child: TabBar(
+            SliverPersistentHeader(
+              delegate: _SliverAppBarDelegate(
+                TabBar(
                   controller: _tabController,
-                  labelStyle: TextStyle(fontSize: screenHeight*0.018),
+                  labelStyle: TextStyle(fontSize: screenHeight * 0.017),
                   tabs: const [
                     Tab(
                       text: 'Role',
                     ),
                     Tab(
-                      text: 'Pernissions',
+                      text: 'Permissions',
                     ),
                     Tab(
                       text: 'Members',
                     ),
                   ],
                   indicator: BoxDecoration(
-                    color: Colors.black54,
+                    color: Colors.black87,
                     borderRadius: BorderRadius.circular(9.0),
                   ),
                   labelColor: Colors.white,
@@ -98,18 +88,101 @@ class _AssignmentScrrenState extends State<AssignmentScrren> with SingleTickerPr
                   physics: BouncingScrollPhysics(),
                 ),
               ),
+              pinned: true,
             ),
-            Container(
-              height: screenHeight * 0.6,
-              child: TabBarView(
-                controller: _tabController,
-                children: [
-                  RoleContent(),
-                  PermissionsContent(),
-                  MembersContent(),
-                ],
+          ];
+        },
+        body: TabBarView(
+          controller: _tabController,
+          children: [
+            RoleContent(),
+            PermissionsContent(),
+            MembersContent(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
+  final TabBar _tabBar;
+
+  _SliverAppBarDelegate(this._tabBar);
+
+  @override
+  double get minExtent => _tabBar.preferredSize.height;
+
+  @override
+  double get maxExtent => _tabBar.preferredSize.height;
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.grey[300],
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(10),
+          child: _tabBar,
+        ),
+      ),
+    );
+  }
+
+  @override
+  bool shouldRebuild(_SliverAppBarDelegate oldDelegate) {
+    return false;
+  }
+}
+
+class PermissionsContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(16.0),
+      child: SingleChildScrollView(
+        physics: BouncingScrollPhysics(),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "General Permissions",
+              style: TextStyle(
+                fontSize: 17,
+                fontWeight: FontWeight.bold,
               ),
             ),
+            SizedBox(height: 16),
+            // Example list items, replace it with your actual data
+            PermissionListItem(name: "Ban Members"),
+            PermissionListItem(name: "Delete others Messages"),
+            PermissionListItem(name: "Pin Messages"),
+            PermissionListItem(name: "Create new Roles"),
+            PermissionListItem(name: "Edit Community Info"),
+            PermissionListItem(name: "Permission 3"),
+            PermissionListItem(name: "Permission 1"),
+            PermissionListItem(name: "Permission 2"),
+            PermissionListItem(name: "Permission 3"),
+            PermissionListItem(name: "Permission 1"),
+            PermissionListItem(name: "Permission 2"),
+            PermissionListItem(name: "Permission 3"),
+            PermissionListItem(name: "Permission 3"),
+            PermissionListItem(name: "Permission 1"),
+            PermissionListItem(name: "Permission 2"),
+            PermissionListItem(name: "Permission 3"),
+            PermissionListItem(name: "Permission 1"),
+            PermissionListItem(name: "Permission 2"),
+            PermissionListItem(name: "Permission 3"),
+            PermissionListItem(name: "Permission 3"),
+            PermissionListItem(name: "Permission 1"),
+            PermissionListItem(name: "Permission 2"),
+            PermissionListItem(name: "Permission 3"),
+            // Add more items as needed
           ],
         ),
       ),
@@ -122,42 +195,6 @@ class RoleContent extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Text("Role content"),
-    );
-  }
-}
-
-class PermissionsContent extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(16.0),
-      child: ListView(
-        physics: BouncingScrollPhysics(),
-        children: [
-          Text(
-            "General Permissions",
-            style: TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          SizedBox(height: 16),
-          // Example list items, replace it with your actual data
-          PermissionListItem(name: "Ban Members"),
-          PermissionListItem(name: "Delete others Messages"),
-          PermissionListItem(name: "Pin Messages"),
-          PermissionListItem(name: "Create new Roles"),
-          PermissionListItem(name: "Edit Community Info"),
-          PermissionListItem(name: "Permission 3"),
-          PermissionListItem(name: "Permission 1"),
-          PermissionListItem(name: "Permission 2"),
-          PermissionListItem(name: "Permission 3"),
-          PermissionListItem(name: "Permission 1"),
-          PermissionListItem(name: "Permission 2"),
-          PermissionListItem(name: "Permission 3"),
-          // Add more items as needed
-        ],
-      ),
     );
   }
 }
@@ -185,26 +222,70 @@ class _PermissionListItemState extends State<PermissionListItem> {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Text(
-          widget.name,
-          style: TextStyle(fontSize: 17, color: Colors.grey),
-        ),
-        Transform.scale(
-          scale: 1.5,
-          child: Switch(
-            value: isSwitched,
-            onChanged: (bool value) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            widget.name,
+            style: TextStyle(fontSize: 17, color: Colors.grey),
+          ),
+          GestureDetector(
+            onTap: () {
               setState(() {
-                isSwitched = value;
+                isSwitched = !isSwitched;
               });
             },
+            child: Container(
+              width: 60,
+              height: 30,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                color: isSwitched ? Colors.blue : Colors.grey[350],
+                border: Border.all(
+                  color: isSwitched ? Colors.blue : Colors.grey[350]!,
+                  width: 2.0,
+                ),
+              ),
+              child: Stack(
+                children: [
+                  AnimatedPositioned(
+                    duration: Duration(milliseconds: 200),
+                    curve: Curves.easeIn,
+                    left: isSwitched ? 30 : 0,
+                    child: AnimatedSwitcher(
+                      duration: Duration(milliseconds: 200),
+                      transitionBuilder:
+                          (Widget child, Animation<double> animation) {
+                        return RotationTransition(
+                          child: child,
+                          turns: animation,
+                        );
+                      },
+                      child: GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            isSwitched = !isSwitched;
+                          });
+                        },
+                        child: Center(
+                          child: Icon(
+                            isSwitched ? Icons.circle : Icons.circle,
+                            color: Colors.white,
+                            size: 26.0,
+                            key: ValueKey<bool>(isSwitched),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
-
